21e9d2fc5449b046035c40e8a574b82dcdba294d
yury-stepanov <advers191@gmail.com>
2018-03-08 23:17:50 +0300

    Upgraded to webpack v4

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
+import './app.css';
+
+const App = () => {
+    return <div>Hello world</div>;
+};
+
+export default App;
+
+ReactDOM.render(<App />, document.getElementById('app'));
