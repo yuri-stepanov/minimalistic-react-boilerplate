45e03ca9c7b84e67b478c99c7eaa3d4de81e6134
yury-stepanov <advers191@gmail.com>
2018-03-08 23:56:57 +0300

    Added ability to resolve from root

--- webpack.config.js
+++ webpack.config.js
@@ -1,52 +1,54 @@
+const { resolve } = require('path');
 const HtmlWebPackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = {
-    devtool: 'inline-source-map',
-    module: {
-        rules: [
-            {
-                test: /\.(js|jsx)$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: 'babel-loader',
-                },
-            },
-            {
-                test: /\.html$/,
-                use: [
-                    {
-                        loader: 'html-loader',
-                        options: { minimize: true },
-                    },
-                ],
-            },
-            {
-                test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
-                loader: 'url-loader',
-                options: {
-                    limit: 50000,
-                    name: '[name].[ext]',
-                },
-            },
-            {
-                test: /\.css$/,
-                use: [MiniCssExtractPlugin.loader, 'css-loader'],
-            },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: 'html-loader',
+            options: { minimize: true },
+          },
         ],
-    },
-    resolve: {
-        extensions: ['.js', '.jsx'],
-    },
-    plugins: [
-        new HtmlWebPackPlugin({
-            template: './static/index.html',
-            filename: './index.html',
-        }),
-        new MiniCssExtractPlugin({
-            // Options similar to the same options in webpackOptions.output
-            // both options are optional
-            filename: '[name].css',
-        }),
+      },
+      {
+        test: /\.(ttf|otf|eot|svg|woff(2)?)(\?[a-z0-9]+)?$/,
+        loader: 'url-loader',
+        options: {
+          limit: 50000,
+          name: '[name].[ext]',
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
     ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    modules: [resolve('./src'), resolve('./node_modules')],
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: './static/index.html',
+      filename: './index.html',
+    }),
+    new MiniCssExtractPlugin({
+      // Options similar to the same options in webpackOptions.output
+      // both options are optional
+      filename: '[name].css',
+    }),
+  ],
 };
